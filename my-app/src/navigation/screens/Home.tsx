55d876f637259219e72a1050259532fa9d37c61b
SANTOS Alexis <alexis.santos@algosup.com>
2025-03-06 12:30:04 +0100

    Remove languages impact on the Home Page

--- my-app/src/navigation/screens/Home.tsx
+++ my-app/src/navigation/screens/Home.tsx
@@ -1,23 +1,14 @@
 import { Button, Text } from '@react-navigation/elements';
-import { FlatList, Modal, Platform, StyleSheet, TouchableOpacity, View } from 'react-native';
+import { Platform, StyleSheet } from 'react-native';
 import { RootView } from '../../components/RootView';
-import i18next, { languageResources } from '../../services/i18next';
 import { useTranslation } from 'react-i18next';
 import { ThemedText } from '../../components/ThemedText';
-import { useState } from 'react';
 import { getThemeColors, useThemeColors } from '../../hooks/useThemeColors';
 
 export function Home() {
-  const [visible, setVisible] = useState(false);
   const { t } = useTranslation();
   const theme = getThemeColors();
   const colors = useThemeColors();
-
-  const chgLanguage = (language: string) => {
-    i18next.changeLanguage(language);
-    setVisible(false);
-  };
-
   return (
     <RootView style={styles.container}>
       <Text>{t("homeScreen")}</Text>
@@ -37,32 +28,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     gap: 10,
-  },
-  languageButton: {
-    flex: 1,
-    ...Platform.select({
-      web: {
-        maxWidth: '25%',
-        height: 200,
-      },
-      default: {
-        maxWidth: '50%',
-        height: 100,
-      },
-    })
-  },
-  languageList: {
-    flex: 1,
-    justifyContent: 'center',
-    marginTop: 50,
-    borderTopLeftRadius: 30,
-    borderTopRightRadius: 30,
-  },
-  languageListHeader: {
-    alignItems: 'center',
-    borderTopLeftRadius: 30,
-    borderTopRightRadius: 30,
-    height: 50,
-    justifyContent: 'center',
   }
 });
