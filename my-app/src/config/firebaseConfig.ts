f2a59de11455aa122aeeea14cc67b757a4e75a5b
Alexis SANTOS <alexis.santos@algosup.com>
2025-05-03 09:32:44 +0200

    Firebase Issue is solved

--- my-app/src/config/firebaseConfig.ts
+++ my-app/src/config/firebaseConfig.ts
@@ -1,23 +1,66 @@
-// Inputs from firebase config for Device and Web
-import { FIREBASE_APP, FIREBASE_AUTH } from './firebaseConfigDevice'
-import { webApp, webAuth, getWebAuth } from './firebaseConfigWeb';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from '@firebase/auth';
+// Import the functions you need from the SDKs you need
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "@firebase/auth";
+import auth from "@react-native-firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+import { Platform } from "react-native";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Define the DeviceApp interface 
+interface WebAuth {
+    signInWithEmailAndPassword: (email: string, password: string) => Promise<any>,
+    createUserWithEmailAndPassword: (email: string, password: string) => Promise<any>,
+}
+
 interface DeviceAuth {
-  signInWithEmailAndPassword: (email: string, password: string) => Promise<any>;
-  createUserWithEmailAndPassword: (email: string, password: string) => Promise<any>;
+    signInWithEmailAndPassword: (email: string, password: string) => Promise<any>,
+    createUserWithEmailAndPassword: (email: string, password: string) => Promise<any>,
 }
 
+let webApp;
+let webAuth: WebAuth | undefined;
+let webAnalytics;
+let getWebAuth: any;
 let deviceApp;
-let deviceAuth : DeviceAuth | undefined;
-let deviceDatabase;
+let deviceAuth: DeviceAuth | undefined;
 
-// Initialize Firebase for Device
-deviceApp = FIREBASE_APP;
-deviceAuth = {
-  signInWithEmailAndPassword: (email: string, password: string) => FIREBASE_AUTH.signInWithEmailAndPassword(email, password),
-  createUserWithEmailAndPassword: (email: string, password: string) => FIREBASE_AUTH.createUserWithEmailAndPassword(email, password) 
+if (Platform.OS === 'web') {
+
+    const firebase = require('firebase/app');
+
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+        apiKey: "AIzaSyCgdSSySYeYY2gt1RSIBMC1GbYG5i6qRfU",
+        authDomain: "mymoonshot-76f99.firebaseapp.com",
+        projectId: "mymoonshot-76f99",
+        storageBucket: "mymoonshot-76f99.firebasestorage.app",
+        messagingSenderId: "887897516773",
+        appId: "1:887897516773:web:6a92dc980492c433b3820c",
+        measurementId: "G-0SVDMRES6F"
+    };
+
+    // Initialize Firebase for Web
+    webApp = firebase.initializeApp(firebaseConfig);
+    webAnalytics = getAnalytics(webApp);
+    getWebAuth = getAuth(webApp);
+    webAuth = {
+        signInWithEmailAndPassword: (email: string, password: string) => signInWithEmailAndPassword(getWebAuth, email, password),
+        createUserWithEmailAndPassword: (email: string, password: string) => createUserWithEmailAndPassword(getWebAuth, email, password)
+    };
+    deviceApp = undefined;
+    deviceAuth = undefined;
+} else {
+    // Initialize Firebase for iOS / Android
+    const firebase = require('@react-native-firebase/app');
+    deviceApp = firebase.initializeApp();
+    deviceAuth = {
+        signInWithEmailAndPassword: (email: string, password: string) => auth().signInWithEmailAndPassword(email, password),
+        createUserWithEmailAndPassword: (email: string, password: string) => auth().createUserWithEmailAndPassword(email, password)
+    };
+    webApp = undefined;
+    webAuth = undefined;
+    webAnalytics = undefined;
+    getWebAuth = undefined;
 }
 
-export { deviceApp, deviceAuth, webApp, webAuth, getWebAuth }
+export { webApp, webAnalytics, getWebAuth, webAuth, deviceApp, deviceAuth };
--- my-app/src/config/firebaseConfigDevice.ts
+++
@@ -1,9 +0,0 @@
-// Import the functions you need from the SDKs you need
-import { getFirestore } from 'firebase/firestore';
-import auth from "@react-native-firebase/auth";
-
-const firebase = require('@react-native-firebase/app');
-
-// Initialize Firebase
-export const FIREBASE_APP = firebase.initializeApp()
-export const FIREBASE_AUTH = auth();
